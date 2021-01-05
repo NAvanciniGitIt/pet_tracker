00ac54e56d7299eb2df64c3e8ea65920973e136b
Tasha <navancini@gmail.com>
2021-01-05 12:30:27 -0500

    --

--- pet_tracker/config/environment.rb
+++ pet_tracker/config/environment.rb
@@ -9,4 +9,6 @@ ActiveRecord::Base.establish_connection(
 )
 
 require './app/controllers/application_controller'
+require './app/controllers/pets_controller'
+require './app/controllers/user_controller'
 require_all 'app'
--- pet_tracker/db/migrate/20210104194929_create_pets.rb
+++ pet_tracker/db/migrate/20210104194929_create_pets.rb
@@ -1,7 +1,10 @@
 class CreatePets < ActiveRecord::Migration
   def change
     create_table :pets do |t|
-
+      t.string :name
+      t.integer :age
+      t.string :breed
+      t.integer :weight
       t.timestamps null: false
     end
   end
