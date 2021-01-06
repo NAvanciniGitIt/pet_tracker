1952bab1d6c71c92aa1a24cb3b27d084412ab868
Tasha <navancini@gmail.com>
2021-01-06 11:59:05 -0500

    and you get a helper

--- pet_tracker/app/controllers/pets_controller.rb
+++ pet_tracker/app/controllers/pets_controller.rb
@@ -2,30 +2,36 @@ require 'date'
 class PetsController < ApplicationController
 
   get '/pet' do
-    @pets = Pet.all
+    redirect_if_not_logged_in
+    @pets = current_user.pets
     @pet = Pet.find_by_id(session[:pet_id])
     erb :'/pets/index'
   end
 
   get '/pet/new' do
+    redirect_if_not_logged_in
     erb :'/pets/new'
   end 
 
   get '/pet/:id' do
+    redirect_if_not_logged_in
       find_pet
+      redirect_if_not_owner
       session[:pet_id] = @pet.id if @pet
       redirect_if_pet_not_found
     erb :'/pets/show'
   end
 
   get '/pet/:id/edit' do
+    redirect_if_not_logged_in
       find_pet
+      redirect_if_not_owner
+      redirect_if_pet_not_found
     erb :'pets/edit'
   end
 
   post '/pet' do
-    pet = Pet.new(params[:pet])
-
+    pet = current_user.pets.build(params[:pet]) #active record association to make a new object based on that association
     if pet.save
       redirect '/pet'
     else
@@ -45,6 +51,8 @@ class PetsController < ApplicationController
 
   delete '/pet/:id' do
     find_pet
+    redirect_if_pet_not_found
+    redirect_if_not_owner
     @pet.destroy if @pet
     redirect '/pet'
   end
@@ -56,6 +64,10 @@ class PetsController < ApplicationController
 
     def redirect_if_pet_not_found
       redirect '/pet' unless @pet
+    end
+
+    def redirect_if_not_owner
+      redirect '/pet' unless @pet.user == current_user
     end
   end
 end
