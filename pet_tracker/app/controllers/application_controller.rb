035d451ce33404ea6bdcbbb37df766e21733ae19
Tasha <navancini@gmail.com>
2021-01-06 11:58:57 -0500

    and you get a helper, and you get a helper

--- pet_tracker/app/controllers/application_controller.rb
+++ pet_tracker/app/controllers/application_controller.rb
@@ -16,6 +16,18 @@ class ApplicationController < Sinatra::Base
     def is_logged_in?
       !!session[:user_id]#is there any user id?
     end
+
+    def redirect_if_logged_in
+      redirect '/games' if is_logged_in?
+    end
+
+    def redirect_if_not_logged_in
+      redirect '/login' unless is_logged_in?
+    end
+
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
   end
 end
 
