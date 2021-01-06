d5240022ed1f7e5d7ac8a560fc2619864eae59e4
Tasha <navancini@gmail.com>
2021-01-06 11:20:44 -0500

    new helper

--- pet_tracker/app/controllers/application_controller.rb
+++ pet_tracker/app/controllers/application_controller.rb
@@ -3,13 +3,19 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
   get "/" do
     erb :welcome
   end
-
-  
+    helpers do
+    def is_logged_in?
+      !!session[:user_id]#is there any user id?
+    end
+  end
 end
+
