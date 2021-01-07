52706f1ea3c822a86a17356e3c6a44a85241f9ea
Tasha <navancini@gmail.com>
2021-01-07 15:04:07 -0500

    add more validations

--- pet_tracker/app/models/user.rb
+++ pet_tracker/app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true 
   validates :password, presence: true
+  validates :name, presence: true
+  validates :lastname, presence: true
+  validates :birthday, presence: true 
+  validates :email, presence: true, uniqueness: true 
 end
